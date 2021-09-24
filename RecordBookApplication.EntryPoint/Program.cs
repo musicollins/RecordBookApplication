9f2bb1246c89089abdf0b6384bbb5c3c8dd978b5
Rafael <rafaelmilanes91@gmail.com>
2021-09-24 10:17:24 +0200

    Add project files.

---
+++ RecordBookApplication.EntryPoint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecordBookApplication.EntryPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
