0618cc8470b4f40a603cd92ac209c7f6644a4373
Rafael <rafaelmilanes91@gmail.com>
2021-09-24 10:20:43 +0200

    Changes

--- RecordBookApplication.EntryPoint/Program.cs
+++ RecordBookApplication.EntryPoint/Program.cs
@@ -6,7 +6,8 @@ namespace RecordBookApplication.EntryPoint
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("HELLO SWEDEN!");
+                
         }
     }
 }
